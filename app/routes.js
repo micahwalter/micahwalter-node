98201f715417fad0da41a483e808f5b4dbf3ad31
Micah Walter <micah@micahwalter.com>
2014-02-27 17:17:06 -0500

    removed some duplicate code

--- app/routes.js
+++ app/routes.js
@@ -33,13 +33,6 @@ module.exports = function(app, passport) {
 
 };
 
-// route middleware to ensure user is logged in
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-};
-
-//////////////////////////////////////////////////////////////////////
 // route middleware to make sure a user is logged in
 
 function isLoggedIn(req, res, next) {
