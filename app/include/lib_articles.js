a35f22738a418ee1dec1f32e01f7435d4071a0ca
Micah Walter <micah@micahwalter.com>
2014-03-17 23:29:33 -0400

    um

---
+++ app/include/lib_articles.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// data model for an article
+var ArticleSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    content: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+});
+
+
+// methods
+ArticleSchema.statics.load = function(id, cb) {
+    this.findOne({
+        _id: id
+    }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Article', ArticleSchema);
