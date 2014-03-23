a1e06afbe8b9ec8243805e3c0dff7c27169006ef
Micah Walter <micah@micahwalter.com>
2014-03-23 15:19:20 -0400

    send a welcome email

--- app/complete_profile.js
+++ app/complete_profile.js
@@ -20,6 +20,12 @@ exports.render = function(req, res) {
 /**
  * Update the users profile
  */
+
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_APIKEY);
+
+var async = false;
+
 exports.update = function(req, res) {
 	
 	User.findByID(req.user.id, function (err, user) {
@@ -34,6 +40,28 @@ exports.update = function(req, res) {
 	                user: user
 	            });
 	        } else {
+				var message = {
+				    "html": "<p>Yo " + user.twitter.displayName + ", how are you?</p>",
+				    "text": "Yo " + user.twitter.displayName + ", how are you?",
+				    "subject": "Welcome to micahwalter.com",
+				    "from_email": "micah@micahwalter.com",
+				    "from_name": "Micah Walter",
+				    "to": [{
+				            "email": user.email,
+				            "name": user.twitter.displayName,
+				            "type": "to"
+				        }],
+				    "headers": {
+				        "Reply-To": "micah@micahwalter.com"
+					}
+				};
+				
+				mandrill_client.messages.send({"message": message, "async": async}, function(result) {
+				    // consider logging this in the db?
+					console.log(result);
+				}, function(e) {
+				    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+				});
 	            return res.redirect('/dashboard');
 	        }
 	    });
