00192463363806272d7e7df9f99c1b0c63e68464
Micah Walter <micah@micahwalter.com>
2014-03-30 15:09:35 -0400

    added wwms to dashboard

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,13 +1,24 @@
 var Post = require('./include/lib_posts');
 
+var cooperhewitt = require('node-cooperhewitt');
+var api_token = process.env.CH_API_KEY;
+
 exports.render = function(req, res, passport) {
 	
-	Post.findAllByUserID(req.user._id, function(err, posts) {
-		res.render('dashboard', {
-			user : req.user,
-			posts : posts, 
-			title: 'Dashboard' 
-		});				
+	var method = 'cooperhewitt.labs.whatWouldMicahSay';
+	var args = {'access_token': api_token};
+	
+	cooperhewitt.call(method, args, function(rsp){
+		Post.findAllByUserID(req.user._id, function(err, posts) {
+			res.render('dashboard', {
+				user : req.user,
+				posts : posts, 
+				title: 'Dashboard',
+				wwms: rsp.micah 
+			});				
+		});
+	    
 	});
+	
 		
 };
