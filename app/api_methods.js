36203ca50fb100674c7e707e3850538583e7ec92
Micah Walter <micah@micahwalter.com>
2014-05-26 11:01:31 -0400

    building api docs

--- app/api_methods.js
+++ app/api_methods.js
@@ -1,5 +1,9 @@
+var methods = require('./include/config_api_methods');
+
 exports.render = function(req, res) {
+		
     res.render('api_methods', {
+		methods: methods.methods,
 		user : req.user, 
 		title: 'api | methods' 
 	});
