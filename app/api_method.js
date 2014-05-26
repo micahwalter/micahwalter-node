b744668e406b585f0def211c8f1e3d975ca6cd20
Micah Walter <micah@micahwalter.com>
2014-05-26 13:07:10 -0400

    more api docs

--- app/api_method.js
+++ app/api_method.js
@@ -1,6 +1,18 @@
+var methods = require('./include/config_api_methods').methods;
+
 exports.render = function(req, res) {
+	
+	for(var method in methods){
+		if ( methods[method].name == req.params['method']){
+			var meth = methods[method];
+		}	
+	}
+	
+	
     res.render('api_method', {
+		method: meth,
 		user : req.user, 
-		title: 'API method' 
+		title: 'api | methods | ' + req.params['method'],
+		 
 	});
 };
