1d029b467ec4be1d304cc5c1c3215e2cebf1966a
Micah Walter <micah@micahwalter.com>
2014-06-06 12:32:08 -0400

    wwms?

--- app/index.js
+++ app/index.js
@@ -1,8 +1,18 @@
+var cooperhewitt = require('node-cooperhewitt');
+var api_token = process.env.CH_API_KEY;
+
+
 exports.render = function(req, res) {
 	if ( req.user )
 		res.redirect('/dashboard');
 	
-    res.render('index', { 
-		title : 'Home'
-	 });
+	var method = 'cooperhewitt.labs.whatWouldMicahSay';
+	var args = {'access_token': api_token};
+	
+	cooperhewitt.call(method, args, function(rsp){
+	    res.render('index', { 
+			title : 'Home',
+			wwms : rsp.micah
+		});
+	});
 };
