ea09058d59d144a6cdc4e4add0153d87b2b397f2
Micah Walter <micah@micahwalter.com>
2014-06-07 13:01:16 -0400

    cleaned up

--- micahwalter.js
+++ micahwalter.js
@@ -15,23 +15,6 @@ require('./config/passport')(passport);
 
 mongoose.connect(configDB.url);
 
-// // Bootstrap models
-// var models_path = __dirname + '/app/include';
-// var walk = function(path) {
-//     fs.readdirSync(path).forEach(function(file) {
-//         var newPath = path + '/' + file;
-//         var stat = fs.statSync(newPath);
-//         if (stat.isFile()) {
-//             if (/(.*)\.(js$|coffee$)/.test(file)) {
-//                 require(newPath);
-//             }
-//         } else if (stat.isDirectory()) {
-//             walk(newPath);
-//         }
-//     });
-// };
-// walk(models_path);
-
 app.configure(function() {
 
 	// set up express
